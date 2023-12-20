4824a15c0d0c241ec5c11a81b2beeeff4d980e6d
Nick Koole <nickkoole@hotmail.com>
2023-12-20 16:41:11 +0100

    Add project files.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Director { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Made Profit")]
+        public bool? MadeProfit { get; set; }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            if (context.Movie.Any())
+            {
+                return;
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    Director = "Rob Reiner",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    MadeProfit = true
+                },
+                new Movie
+                {
+                    Title = "The Lord of the Rings: The Fellowship of the Ring",
+                    Director = "Peter Jackson",
+                    ReleaseDate = DateTime.Parse("2002-12-19"),
+                    MadeProfit = true
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    Director = "Ivan Reitman",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    MadeProfit = true
+                },
+                new Movie
+                {
+                    Title = "The Matrix",
+                    Director = "The Wachowskis",
+                    ReleaseDate = DateTime.Parse("1999-6-17"),
+                    MadeProfit = true
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
